65aeda8edf5c42a087f2838b7720127a8044e7d2
glowinska <anna.glowinska98@gmail.com>
2021-02-02 14:23:38 +0100

    improved detail page

--- app/src/main/java/com/example/android/marsrealestate/detail/DetailFragment.kt
+++ app/src/main/java/com/example/android/marsrealestate/detail/DetailFragment.kt
@@ -16,11 +16,13 @@
 
 package com.example.android.marsrealestate.detail
 
+
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.android.marsrealestate.databinding.FragmentDetailBinding
 
 /**
@@ -30,10 +32,14 @@ class DetailFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
 
-        @Suppress("UNUSED_VARIABLE")
         val application = requireNotNull(activity).application
         val binding = FragmentDetailBinding.inflate(inflater)
         binding.lifecycleOwner = this
+
+        //val marsProperty = DetailFragmentArgs.fromBundle(arguments!!).selectedProperty
+        //val viewModelFactory = DetailViewModelFactory(marsProperty, application)
+        //
+
         return binding.root
     }
 }
